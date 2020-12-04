cd1bf15dc9edb43010415aaa5cd739b818042546
yy <churchee@126.com>
2020-12-04 08:45:24 +0800

    add(函数)

--- function/3.1.py
+++ function/3.1.py
@@ -1,5 +1,7 @@
 import random
-i
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s-%(name)s-%(levelname)s-%(message)s')
 
 def hello():
     print('hello!你好!')
@@ -12,17 +14,20 @@ def getAnswer(number):
         return f'数字是{number}!'
 
 def big_or_small():
-    print(请输入一个数字：)
+    print('请输入一个数字：')
     number = input()
     r = random.randint(1, 100)
     answer = getAnswer(r)
-    if number < answer:
-        print(太小了)
-    getAnswer()
+    if int(number) < int(r):
+        logging.debug(number)
+        logging.debug(answer)
+        print('太小了')
+    
 
 if __name__ == '__main__':
     hello()
     hello()
     hi('yy')
     hi('hbpu')
+    big_or_small()
 
